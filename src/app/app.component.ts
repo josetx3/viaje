66b57c0c4cf92e173cfeedb87f3685282ae28de1
josetx3 <joseleoquintero123@gmail.com>
2025-07-07 17:28:16 -0500

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'viaje';
+export class AppComponent implements OnInit {
+  diasRestantes!: number;
+  horasRestantes!: number;
+  minutosRestantes!: number;
+  segundosRestantes!: number;
+
+  ngOnInit() {
+    this.calcularTiempoRestante();
+    setInterval(() => {
+      this.calcularTiempoRestante();
+    }, 1000); // actualiza cada segundo
+  }
+
+  calcularTiempoRestante() {
+    const ahora = new Date();
+    const destino = new Date(ahora.getFullYear(), 8, 20, 0, 0, 0); // 8 = septiembre
+
+    if (ahora > destino) {
+      this.diasRestantes = 0;
+      this.horasRestantes = 0;
+      this.minutosRestantes = 0;
+      this.segundosRestantes = 0;
+      return;
+    }
+
+    const diferencia = destino.getTime() - ahora.getTime(); // en milisegundos
+
+    this.diasRestantes = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    this.horasRestantes = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    this.minutosRestantes = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    this.segundosRestantes = Math.floor((diferencia % (1000 * 60)) / 1000);
+  }
 }
